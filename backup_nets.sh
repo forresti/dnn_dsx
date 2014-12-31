915c3e59c7a2964ef0d63e4cf52c50d51da4001f
Forrest Iandola <forresti@eecs.berkeley.edu>
2014-12-31 18:52:02 -0500

    backup nets so they don't get eaten by filesystem purges

---
+++ backup_nets.sh
@@ -0,0 +1,21 @@
+
+mkdir nets_backup
+
+#for d in ./nets/*
+for((i=0; i<1000; i++))
+do
+  d=./nets/$i
+  output=./nets_backup/$i
+  mkdir $output
+
+  newestState=`ls -t $d/*solverstate | head -1` #thx: stackoverflow.com/questions/5885934
+  newestModel=`ls -t $d/*caffemodel | head -1` 
+
+  echo $newestState
+
+  cp $newestState $output
+  cp $newestModel $output
+  cp $d/*.log* $output
+  cp $d/*prototxt $output
+done
+
