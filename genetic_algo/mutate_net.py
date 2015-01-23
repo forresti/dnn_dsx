2ea4d889552cb3c4e16839e8ab6a6ced3988bc64
forresti <fiandola@gmail.com>
2015-01-23 14:45:32 -0800

    fleshing this out

--- genetic_algo/mutate_net.py
+++ genetic_algo/mutate_net.py
@@ -21,16 +21,19 @@ class NetMutator:
 
     def __init__(self):
         self.ranges = dict()
+        #TODO: make this an ordered dict so that 'kernel_size' comes before 'stride'
         self.ranges['conv'] = dict()
         self.ranges['pool'] = dict()
 
         self.ranges['conv']['num_output'] = [64, 128, 256, 512]
         self.ranges['conv']['kernel_size'] = [1,2,3,4,5,7,9,11]
-        self.ranges['conv']['stride'] = self.ranges['conv']['kernel_size'] #and, when selecting stride, make sure it's <= kernel_size.
+        #self.ranges['conv']['stride'] = self.ranges['conv']['kernel_size'] #and, when selecting stride, make sure it's <= kernel_size.
+        self.ranges['conv']['stride'] = [1,2,3,4]
 
-        self.ranges['pool']['type'] = [0, 1] #['MAX', 'AVE']
+        self.ranges['pool']['pool'] = [0, 1] #['MAX', 'AVE']
         self.ranges['pool']['kernel_size'] = [1,2,3,4,5,7,9,11]
-        self.ranges['pool']['stride'] = self.ranges['pool']['kernel_size'] 
+        #self.ranges['pool']['stride'] = self.ranges['pool']['kernel_size'] 
+        self.ranges['pool']['stride'] = [1,2,3,4]
 
         #roughly 24 hyperparams that we play with in VGG-F. on average, mutate 2 params per net.
         self.mutation_prob = 1.0 / 12.0 
@@ -38,7 +41,6 @@ class NetMutator:
     #@param hp = hyperparam to modify (e.g. 'stride')
     # this updates 'net.' (passed by ref everywhere...)
     def mutate_layer(self, net, layerName, hp):
-        #TODO
         L = getLayerByName(net, layerName)
         if L.HasField('pooling_param'):
             param_ = 'pooling_param'
@@ -52,14 +54,23 @@ class NetMutator:
 
         hp_old_value = getattr(getattr(L, param_), hp) #e.g. net.layers[0].convolution_param.stride 
 
-        if hp != 'stride':
-            my_range = [v for v in self.ranges[layerType][hp] if v != hp_old_value] #don't mutate back to prev value. (TODO: test this)
-            hp_new_value = choice(my_range)
-            setattr(getattr(L, param_), hp, hp_new_value) # "net.layers[0].convolution_param.stride = hp_new_value"
+        #choose new value for hyperparam.
+        my_range = [v for v in self.ranges[layerType][hp] if v != hp_old_value] #don't mutate back to prev value.
+        if hp == 'stride':
+            my_kernel_size = getattr(getattr(L, param_), 'kernel_size')
+            my_range = [v for v in my_range if v <= my_kernel_size] #ensure stride <= kernel_size
+        hp_new_value = choice(my_range)
 
-        #else: #hp == 'stride'
-        #TODO        
+        #save new hp value to net 
+        setattr(getattr(L, param_), hp, hp_new_value) # "net.layers[0].convolution_param.stride = hp_new_value"
+        print 'updated %s: %s = %d' %(L.name, hp, hp_new_value)
 
+        #if kernel_size is now larger than stride, decrement stride until stride <= kernel_size
+        if hp == 'kernel_size' and hp_new_value < getattr(getattr(L, param_), 'stride'):
+            stride_range = [v for v in self.ranges[layerType]['stride'] if v <= hp_new_value] #strides that are less than new kernel_size
+            new_stride = stride_range[-1]
+            setattr(getattr(L, param_), 'stride', new_stride) 
+            print 'updated %s: %s = %d' %(L.name, 'stride', new_stride)
 
     #@param net = trainval.prototxt loaded into NetParameter structure
     #@return new_net = mutated net, which you can write to prototxt file
@@ -82,13 +93,19 @@ class NetMutator:
 
 #TODO: make the following into a function that takes path to a net to mutate.
 if __name__ == '__main__':
-    seed(1) #TODO: take from cmd-line
+    #seed(4) #TODO: take from cmd-line
 
     net = caffe_pb2.NetParameter()
-    net_file = "/media/big_disk/installers_old/caffe-bvlc-master/models/bvlc_alexnet/deploy.prototxt"
+    net_file = "/media/big_disk/installers_old/caffe-bvlc-master/models/bvlc_alexnet/train_val.prototxt"
     net_str = open(net_file).read()
     text_format.Merge(net_str, net) #net = load net from str to protobuf
 
     mutator = NetMutator()
     mutator.mutate_layers(net)
-    embed()
+    #embed()
+
+    out_net_file = './trainval_mutated.prototxt'
+    f = open(out_net_file, 'w')
+    f.write(text_format.MessageToString(net))
+    f.flush()
+    f.close()
