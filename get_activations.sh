bc85f60ec8acfdbd1e4d1ee91fa249c8d9c93d1a
forresti <forresti@eecs.berkeley.edu>
2015-08-03 15:58:02 -0700

    got activations

--- get_activations.sh
+++ get_activations.sh
@@ -1,13 +1,14 @@
 
 caffe_root=/home/eecs/forresti/__caffe_allreduce
 now=`date +%a_%Y_%m_%d__%H_%M_%S`
-#d=$1 #you pass in directory of net 
-d=NiN_batch256_stepsize50k_maxiter110k_fromCompiler_withDropout_fixWeightInit/
-weights=$d/nin_imagenet_train_iter_110000.caffemodel
-#weights=$d/alexnet_finetune_iter_20000.caffemodel #TODO: pick newest one.
-#iter=35 # number of batches (1750/50)
+g=2
+d=$1 #you pass in directory of net 
+#d=nets_multiGPU/NiN_batch256_stepsize50k_maxiter110k_fromCompiler_withDropout_fixWeightInit
+weights=`ls -t $d/*caffemodel | head -1`
+#weights=$d/nin_imagenet_train_iter_110000.caffemodel
 iter=782 # ceil(50000/64)
 output_blob=pool4 #TODO: auto-select final blob
+#output_blob=fc8
 output_db_path=$d/activations_lmdb
 
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
@@ -18,6 +19,6 @@ output_db_path=$d/activations_lmdb
 #$caffe_root/build/tools/extract_features $weights $d/trainval.prototxt fc8 $output 35 lmdb GPU 0
 
 $caffe_root/build/tools/extract_features $weights $d/trainval.prototxt $output_blob \
-  $output_db_path $iter lmdb GPU 0 \
+  $output_db_path $iter lmdb GPU $g \
   > $d/get_activations_$now.log 2>&1 &
 
