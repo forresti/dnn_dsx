563ff8c45d0347dc4d19a6f1523bc2383098658d
forresti <forresti@eecs.berkeley.edu>
2015-08-03 17:25:36 -0700

    minor tweaks

--- get_activations.sh
+++ get_activations.sh
@@ -1,15 +1,19 @@
 
 caffe_root=/home/eecs/forresti/__caffe_allreduce
 now=`date +%a_%Y_%m_%d__%H_%M_%S`
-g=2
+g=4
 d=$1 #you pass in directory of net 
 #d=nets_multiGPU/NiN_batch256_stepsize50k_maxiter110k_fromCompiler_withDropout_fixWeightInit
 weights=`ls -t $d/*caffemodel | head -1`
 #weights=$d/nin_imagenet_train_iter_110000.caffemodel
 iter=782 # ceil(50000/64)
-output_blob=pool4 #TODO: auto-select final blob
+#output_blob=pool4 #TODO: auto-select final blob
 #output_blob=fc8
-output_db_path=$d/activations_lmdb
+output_blob=prob
+#output_db_path=$d/activations_lmdb
+output_db_path=$d/activations_prob_lmdb
+
+rm -rf $output_db_path
 
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/orpix/thirdparty/cudnn-6.5-linux
@@ -18,7 +22,7 @@ output_db_path=$d/activations_lmdb
 
 #$caffe_root/build/tools/extract_features $weights $d/trainval.prototxt fc8 $output 35 lmdb GPU 0
 
-$caffe_root/build/tools/extract_features $weights $d/trainval.prototxt $output_blob \
-  $output_db_path $iter lmdb GPU $g \
+CUDA_VISIBLE_DEVICES=$g $caffe_root/build/tools/extract_features $weights $d/trainval.prototxt $output_blob \
+  $output_db_path $iter lmdb GPU 0 \
   > $d/get_activations_$now.log 2>&1 &
 
