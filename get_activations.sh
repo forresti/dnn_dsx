5dd361acbec537afee01ca8c59b869b94ba51ead
forresti <forresti@eecs.berkeley.edu>
2015-08-12 14:05:54 -0700

    gpu id as cmdline

--- get_activations.sh
+++ get_activations.sh
@@ -1,12 +1,12 @@
 
 caffe_root=/home/eecs/forresti/__caffe_allreduce
 now=`date +%a_%Y_%m_%d__%H_%M_%S`
-g=4
+g=$2
 d=$1 #you pass in directory of net 
 #d=nets_multiGPU/NiN_batch256_stepsize50k_maxiter110k_fromCompiler_withDropout_fixWeightInit
 weights=`ls -t $d/*caffemodel | head -1`
-#weights=$d/nin_imagenet_train_iter_110000.caffemodel
-iter=782 # ceil(50000/64)
+#iter=782 # ceil(50000/64)
+iter=1563 #50000/32
 #output_blob=pool4 #TODO: auto-select final blob
 #output_blob=fc8
 output_blob=prob
@@ -14,9 +14,6 @@ output_blob=prob
 output_db_path=$d/activations_prob_lmdb
 
 rm -rf $output_db_path
-
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/orpix/thirdparty/cudnn-6.5-linux
 
 #Usage: extract_features  pretrained_net_param  feature_extraction_proto_file  extract_feature_blob_name1[,name2,...]  save_feature_dataset_name1[,name2,...]  num_mini_batches  db_type  [CPU/GPU] [DEVICE_ID=0]
 
