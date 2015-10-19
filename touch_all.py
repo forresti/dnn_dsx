0305fd7cf9512c9ff09f1143e017bc6e6ff15994
Forrest Iandola <forresti@eecs.berkeley.edu>
2015-10-19 15:19:06 -0400

    touch_all

--- touch_all.py
+++ touch_all.py
@@ -1,9 +1,17 @@
 import os
+from time import sleep
 
 #recursive touch
 def touch_r(path):
-    os.system('find %s -exec touch -h {} \;' %path)
+  os.system('touch %s/*' %path)
+  os.system('find %s -exec touch -h {} \;' %path) 
 
 if __name__ == "__main__":
+
+  while 1:
     touch_r('.')
-    touch_r('$MEMBERWORK/csc103/dnn_exploration/')
+    touch_r('$MEMBERWORK/csc103/dnn_exploration/') 
+    sleep(60*60*24*7) #sleep 1 week, then do it again
+    
+
+
