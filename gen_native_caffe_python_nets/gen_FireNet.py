736986753a7ace32572eda149810535cbca39e62
forresti <fiandola@gmail.com>
2015-11-18 22:26:22 -0800

    dropout

--- gen_native_caffe_python_nets/gen_FireNet.py
+++ gen_native_caffe_python_nets/gen_FireNet.py
@@ -102,7 +102,7 @@ def FireNet(batch_size):
         if curr_bottom in pool_after.keys():
             curr_bottom = FireNet_pooling_layer(n, curr_bottom, pool_after[curr_bottom], layer_idx) 
 
-
+    n.tops['drop'+str(layer_idx)] = L.Dropout(n.tops[curr_bottom], dropout_ratio=0.5, in_place=True)
     n.tops['conv_final'] = L.Convolution(n.tops[curr_bottom], kernel_size=1, pad=1, num_output=1000, weight_filler=dict(type='xavier'))   
     n.tops['relu_conv_final'] = L.ReLU(n.tops['conv_final'], in_place=True) 
     n.tops['pool_final'] = L.Pooling(n.tops['conv_final'], global_pooling=1, pool=P.Pooling.AVE)
