9b53e37da0f97ba9af601b5a6385b34c79f20deb
forresti <forresti@eecs.berkeley.edu>
2015-11-27 14:53:54 -0800

    option of incrementing every 'so many' layers ... 'incr_freq'

--- gen_native_caffe_python_nets/play_with_dims.py
+++ gen_native_caffe_python_nets/play_with_dims.py
@@ -1,3 +1,4 @@
+from math import floor
 from pprint import pprint
 
 def compute_params_size(layer_dims):
@@ -22,21 +23,23 @@ if __name__ == "__main__":
     s['base_1x1_2'] = 128 #TODO: vary... these are cheap.
     s['base_3x3_2'] = 64
 
+    s['incr_freq'] = 1 #increase # filters after every 'this many' layers.
     s['incr_add_1x1_1'] = 128
     s['incr_add_1x1_2'] = 128 #TODO: vary... these are cheap.
     s['incr_add_3x3_2'] = 64
 
-    #FireNet w/ 10 fire layers
     '''
+    #FireNet w/ 10 fire layers
     s['num_fire_layers']=10
 
-    s['base_1x1_1'] = 128
-    s['base_1x1_2'] = 128 #TODO: vary... these are cheap.
-    s['base_3x3_2'] = 64
+    s['base_1x1_1'] = 64
+    s['base_1x1_2'] = 64 
+    s['base_3x3_2'] = 32
 
+    s['incr_freq'] = 2 #increase # filters after every 'this many' layers.
     s['incr_add_1x1_1'] = 64
-    s['incr_add_1x1_2'] = 64 #TODO: vary... these are cheap.
-    s['incr_add_3x3_2'] = 32
+    s['incr_add_1x1_2'] = 64 
+    s['incr_add_3x3_2'] = 16
     '''
 
     layer_dims=dict()
@@ -44,9 +47,9 @@ if __name__ == "__main__":
     prev_chans_out=3 #rgb
     for layer_idx in xrange(0, s['num_fire_layers']):
         base_name = "fire%d/" %layer_idx
-        num_1x1_1 = s['base_1x1_1'] + layer_idx*s['incr_add_1x1_1']
-        num_1x1_2 = s['base_1x1_2'] + layer_idx*s['incr_add_1x1_2']
-        num_3x3_2 = s['base_3x3_2'] + layer_idx*s['incr_add_3x3_2'] 
+        num_1x1_1 = s['base_1x1_1'] + floor(layer_idx/s['incr_freq']) * s['incr_add_1x1_1']
+        num_1x1_2 = s['base_1x1_2'] + floor(layer_idx/s['incr_freq']) * s['incr_add_1x1_2']
+        num_3x3_2 = s['base_3x3_2'] + floor(layer_idx/s['incr_freq']) * s['incr_add_3x3_2'] 
 
         layer_dims[base_name+'conv1x1_1'] = {'chans_in':prev_chans_out, 'chans_out':num_1x1_1, 'h':1, 'w':1}
         layer_dims[base_name+'conv1x1_2'] = {'chans_in':num_1x1_1, 'chans_out':num_1x1_2, 'h':1, 'w':1}
@@ -58,5 +61,5 @@ if __name__ == "__main__":
 
     pprint(s)
     pprint(layer_dims)
-    print 'total params size:', total_params_size
+    print 'total params size:', total_params_size, 'MB'
     
