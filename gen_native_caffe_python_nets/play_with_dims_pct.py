2c15bfdafc89cc5fe7a7228247c3f7de0c779751
forresti <fiandola@gmail.com>
2015-12-11 17:37:09 -0800

    playing with ratios and percentages to derive (1x1_1, 1x1_2, 3x3_1) from slightly higher-level knobs.

--- gen_native_caffe_python_nets/play_with_dims_pct.py
+++ gen_native_caffe_python_nets/play_with_dims_pct.py
@@ -16,54 +16,35 @@ if __name__ == "__main__":
 
     s = dict() #settings
 
-    '''
-    #FireNet_default (not including beginning and ending conv layers)
-    s['num_fire_layers']=4
-
-    s['base_1x1_1'] = 128
-    s['base_1x1_2'] = 128 #TODO: vary... these are cheap.
-    s['base_3x3_2'] = 64
-
-    s['incr_freq'] = 1 #increase # filters after every 'this many' layers.
-    s['incr_add_1x1_1'] = 128
-    s['incr_add_1x1_2'] = 128 #TODO: vary... these are cheap.
-    s['incr_add_3x3_2'] = 64
-    '''
-
     #FireNet w/ 8 fire layers
     s['num_fire_layers']=8
 
-    s['base_1x1_1'] = 64
-    s['base_1x1_2'] = 256
-    s['base_3x3_2'] = 64
-
     s['incr_freq'] = 2 #increase # filters after every 'this many' layers.
-    s['incr_add_1x1_1'] = 64
-    s['incr_add_1x1_2'] = 256 
-    s['incr_add_3x3_2'] = 64
+    s['base_expand'] = 64 #number of (1x1_2 + 3x3_2) filters
+    s['incr_expand'] = 64 
 
-    '''    
-    #FireNet w/ 10 fire layers
-    s['num_fire_layers']=10
+    s['3x3_expand_pct'] = 0.25 #percentage of (1x1_2 + 3x3_2) that are 3x3_2
+    s['compress_expand_ratio'] = 1.0 #ratio of 1x1_1 / (1x1_2 + 3x3_2)
 
-    s['base_1x1_1'] = 64
-    s['base_1x1_2'] = 64 
-    s['base_3x3_2'] = 32
-
-    s['incr_freq'] = 2 #increase # filters after every 'this many' layers.
-    s['incr_add_1x1_1'] = 64
-    s['incr_add_1x1_2'] = 64 
-    s['incr_add_3x3_2'] = 16
-    '''
+    #FireNet w/ 10 fire layers...
+    #TODO
 
     layer_dims=dict()
 
+    #TODO: assert(0 <= s['3x3_expand_pct'] <= 1)
+    #TODO: assert(0 <= s['compress_expand_ratio'] <= 1)
     prev_chans_out=3 #rgb
     for layer_idx in xrange(0, s['num_fire_layers']):
         base_name = "fire%d/" %layer_idx
-        num_1x1_1 = s['base_1x1_1'] + floor(layer_idx/s['incr_freq']) * s['incr_add_1x1_1']
-        num_1x1_2 = s['base_1x1_2'] + floor(layer_idx/s['incr_freq']) * s['incr_add_1x1_2']
-        num_3x3_2 = s['base_3x3_2'] + floor(layer_idx/s['incr_freq']) * s['incr_add_3x3_2'] 
+        #num_1x1_1 = s['base_1x1_1'] + floor(layer_idx/s['incr_freq']) * s['incr_add_1x1_1']
+        #num_1x1_2 = s['base_1x1_2'] + floor(layer_idx/s['incr_freq']) * s['incr_add_1x1_2']
+        #num_3x3_2 = s['base_3x3_2'] + floor(layer_idx/s['incr_freq']) * s['incr_add_3x3_2'] 
+
+        num_expand = s['base_expand'] + floor(layer_idx/s['incr_freq']) * s['incr_expand']
+        num_3x3_2 = int(floor(s['3x3_expand_pct'] * num_expand))
+        num_1x1_2 = num_expand - num_3x3_2
+        num_1x1_1 = s['compress_expand_ratio'] * num_expand 
+
 
         layer_dims[base_name+'conv1x1_1'] = {'chans_in':prev_chans_out, 'chans_out':num_1x1_1, 'h':1, 'w':1}
         layer_dims[base_name+'conv1x1_2'] = {'chans_in':num_1x1_1, 'chans_out':num_1x1_2, 'h':1, 'w':1}
@@ -77,6 +58,6 @@ if __name__ == "__main__":
     total_params_size = compute_params_size(layer_dims) #layer_dims is updated in-place
 
     #pprint(s)
-    #pprint(layer_dims)
+    pprint(layer_dims)
     print 'total params size:', total_params_size, 'MB'
     
